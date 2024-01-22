0e792964c87c6453964014f66b5f1ac9a568855b
Parkhi <parkhigoyal46@gmail.com>
2024-01-22 12:58:07 +0530

    category wise storage calculation completed

---
+++ app/src/main/java/com/notesapp/compressify/domain/useCase/GetCategoryStorageUseCase.kt
@@ -0,0 +1,91 @@
+package com.notesapp.compressify.domain.useCase
+
+import com.notesapp.compressify.CompressApplication
+import com.notesapp.compressify.R
+import com.notesapp.compressify.domain.model.CategoryModel
+import com.notesapp.compressify.util.getAllAudioFilesSize
+import com.notesapp.compressify.util.getAllVideoFilesSize
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import javax.inject.Inject
+
+class GetCategoryStorageUseCase @Inject constructor() : BaseUseCase<BaseUseCase.Parameters, List<CategoryModel>>() {
+
+    override suspend fun launch(parameters: Parameters): List<CategoryModel> {
+        val categoryModels = coroutineScope {
+           CategoryModel.CategoryType.values().map {
+                async {
+                    createCategoryModel(it)
+                }
+            }.awaitAll().run {
+                filterNot {
+                    it.categoryType == CategoryModel.CategoryType.OTHER
+                }.sortedByDescending { it.size } + filter {
+                    it.categoryType == CategoryModel.CategoryType.OTHER
+                }
+           }
+        }
+        var startingPoint = 0f
+        var endingPoint = 0f
+        val totalOccupiedSize = categoryModels.sumOf { it.size.toDouble() }
+        for (category in categoryModels) {
+            val size = (category.size * 100) / totalOccupiedSize
+            endingPoint += size.toFloat()
+            category.startingPoint = startingPoint
+            category.endPoint = endingPoint
+            startingPoint = endingPoint
+        }
+        return categoryModels
+    }
+
+    private fun createCategoryModel(categoryType: CategoryModel.CategoryType): CategoryModel {
+        return when (categoryType) {
+            CategoryModel.CategoryType.VIDEO -> {
+                val size = CompressApplication.appContext.getAllVideoFilesSize()
+                val icon = R.drawable.ic_category_video
+                CategoryModel(
+                    icon,
+                    categoryType,
+                    size
+                )
+            }
+            CategoryModel.CategoryType.AUDIO -> {
+                val size = CompressApplication.appContext.getAllAudioFilesSize()
+                val icon = R.drawable.ic_category_audio
+                CategoryModel(
+                    icon,
+                    categoryType,
+                    size
+                )
+            }
+            CategoryModel.CategoryType.IMAGE -> {
+                val size = CompressApplication.appContext.getAllAudioFilesSize()
+                val icon = R.drawable.ic_category_image
+                CategoryModel(
+                    icon,
+                    categoryType,
+                    size
+                )
+            }
+            CategoryModel.CategoryType.DOCUMENT -> {
+                val size = CompressApplication.appContext.getAllAudioFilesSize()
+                val icon = R.drawable.ic_category_document
+                CategoryModel(
+                    icon,
+                    categoryType,
+                    size
+                )
+            }
+            CategoryModel.CategoryType.OTHER -> {
+                val size = CompressApplication.appContext.getAllAudioFilesSize()
+                val icon = R.drawable.ic_category_other
+                CategoryModel(
+                    icon,
+                    categoryType,
+                    size
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/notesapp/compressify/ui/components/home/HomeScreen.kt
+++ app/src/main/java/com/notesapp/compressify/ui/components/home/HomeScreen.kt
@@ -1,38 +1,121 @@
 package com.notesapp.compressify.ui.components.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.notesapp.compressify.domain.model.CategoryModel
 import com.notesapp.compressify.ui.components.home.common.BottomBar
+import com.notesapp.compressify.ui.components.home.common.CustomCircularProgress
+import com.notesapp.compressify.ui.theme.primaryColor
+import com.notesapp.compressify.ui.theme.primaryTintedColor
+import com.notesapp.compressify.util.FileUtil
 import com.notesapp.compressify.util.UIEvent
+import com.notesapp.compressify.util.getFormattedSize
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, onUIEvent: (UIEvent) -> Unit) {
-    Scaffold(
-        modifier = modifier,
-        topBar = {},
-        bottomBar = {
-            BottomBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .defaultMinSize(minHeight = 56.dp),
-                onUIEvent = onUIEvent
-            )
-        }) {
-        Surface(
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    categories: List<CategoryModel>,
+    onUIEvent: (UIEvent) -> Unit
+) {
+    var showMore by remember { mutableStateOf(false) }
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(
             modifier = Modifier
-                .padding(it)
-                .fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 6.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            CustomCircularProgress(
+                progress = 64.4f,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(64.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "Total Space:", style = MaterialTheme.typography.bodyLarge)
+                    Spacer(modifier = Modifier.width(2.dp))
+                    Text(
+                        text = FileUtil.totalStorageSize.getFormattedSize(),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = Bold
+                        )
+                    )
 
+                }
+                Spacer(modifier = Modifier.height(6.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = "Free Space:", style = MaterialTheme.typography.bodyLarge)
+                    Spacer(modifier = Modifier.width(2.dp))
+                    Text(
+                        text = FileUtil.availableStorageSize.getFormattedSize(),
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            color = primaryColor,
+                            fontWeight = Bold
+                        )
+                    )
+                }
+            }
+        }
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+            Text(
+                text = if(showMore) "Show Less" else "Show More",
+                style = MaterialTheme.typography.bodyLarge,
+                color = primaryColor,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { showMore = !showMore }
+
+            )
+        }
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(color = primaryTintedColor)
+        )
+        AnimatedVisibility(visible = showMore) {
+            StorageCategories(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                categoryModelList = categories
+            )
         }
     }
+
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen(categories = listOf(), onUIEvent = {}, modifier = Modifier.fillMaxSize())
 }
